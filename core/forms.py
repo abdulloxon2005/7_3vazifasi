61e8c878f4bc96442516561917a1e0f81b4873c8
Abdulloxon Roziyev <roziyevabdulloxon8@gmail.com>
2025-11-07 16:20:15 +0500

    bajarildi

--- core/forms.py
+++ core/forms.py
@@ -4,3 +4,4 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
     remember_me = forms.BooleanField(required=False,widget=forms.CheckboxInput())
+ 
